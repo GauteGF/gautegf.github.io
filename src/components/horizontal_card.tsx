a54bb32269fd7ef0851b44faa05ae7025970727c
Gaute Flægstad <Gaute.Flaegstad@nhn.no>
2024-06-17 08:29:16 +0200

    feat: added component for a conent-card

---
+++ src/components/horizontal_card.tsx
@@ -0,0 +1,49 @@
+import { Link } from '@chakra-ui/next-js';
+import { Button, Heading, Image, Stack, Text, Card, CardBody, CardFooter } from '@chakra-ui/react'
+
+// Make props optional
+interface HorizontalCardProps {
+  title?: string;
+  imageUrl?: string;
+  imageAlt?: string;
+  description?: string;
+}
+
+export default function HorizontalCard({
+  title = 'Default Title', // Default value for title
+  imageUrl = 'https://via.placeholder.com/200', // Default value for imageUrl
+  imageAlt = 'Default Image', // Default value for imageAlt
+  description = 'Default description' // Default value for description
+}: HorizontalCardProps) {
+    return (
+    <Card
+    direction={{ base: 'column', sm: 'row' }}
+    overflow='hidden'
+    variant='outline'
+    >
+    <Image
+        objectFit='cover'
+        maxW={{ base: '100%', sm: '200px' }}
+        src={imageUrl}
+        alt={imageAlt}
+    />
+
+    <Stack>
+        <CardBody>
+        <Heading size='md'>{title}</Heading>
+
+        <Text py='2'>
+            {description}
+        </Text>
+        </CardBody>
+
+        <CardFooter>
+        <Link href={'https://folk.ntnu.no/gautegf/Bifrost_launch.html'} isExternal>
+            <Button variant='solid' colorScheme='blue'>
+                Go to project
+            </Button>
+        </Link>
+        </CardFooter>
+    </Stack>
+    </Card>
+)}
