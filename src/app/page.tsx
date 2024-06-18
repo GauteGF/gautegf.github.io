ffb4547e07c2f0a6f9f7491fd6d0895769daeff4
Gaute Flægstad <Gaute.Flaegstad@nhn.no>
2024-06-18 13:22:54 +0200

    fix: fixed the home page to take in page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 import SidebarWithHeader from '@/components/sidebar'
 import SmallWithSocial from '@/components/footer'
+import PageContent from './page_content'
 export default function Home() {
   return (
     <main>
-      <SidebarWithHeader/>
+      <SidebarWithHeader pageContent={PageContent}/>
       <SmallWithSocial />
     </main>
   );
