b2bae4d0524842056603cdddc1a8dce3a4bb3b8b
Gaute Greiff Flagstad <gautegf@stud.ntnu.no>
2024-07-03 22:41:44 +0200

    feat: added default structure to projects site

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { Grid, GridItem, Text} from '@chakra-ui/react'
+import Navbar from '@/components/navbar'
+export default function Home() {
+  return (
+    <main>
+      <Grid
+        templateAreas={`"nav main"
+                        "nav footer"`}
+        gridTemplateRows={'100vh 1fr 0px'}
+        gridTemplateColumns={'23vw 1fr'}
+        h='100vh'
+        color='blackAlpha.700'
+        fontWeight='bold'
+      >
+        <Navbar />
+        <GridItem pl='2' bg='green.300' area={'main'}>
+          <Text fontSize={'xl'}>PROJECTS</Text>
+        </GridItem>
+      </Grid>
+    </main>
+  );
+}
