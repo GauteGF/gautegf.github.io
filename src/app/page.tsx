c76adbc160569cc2431a3f076db048d1124bd8a1
Gaute Greiff Flagstad <gautegf@stud.ntnu.no>
2024-07-03 22:40:28 +0200

    fix: removed footer and fixed sidebar size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,14 @@ export default function Home() {
   templateAreas={`"nav main"
                   "nav footer"`}
   gridTemplateRows={'100vh 1fr 0px'}
-  gridTemplateColumns={'25vw 1fr'}
+  gridTemplateColumns={'23vw 1fr'}
   h='100vh'
   color='blackAlpha.700'
   fontWeight='bold'
 >
   <Navbar />
-  <GridItem pl='2' bg='green.300' area={'main'}>
+  <GridItem pl='2' bg='#31363F' area={'main'}>
     Main
-  </GridItem>
-  <GridItem pl='2' bg='blue.300' area={'footer'}>
-    Footer
   </GridItem>
 </Grid>
     </main>
