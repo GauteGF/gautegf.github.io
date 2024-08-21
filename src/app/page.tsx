73f666b42f77e41cf22b6024653b343599256588
Gaute Greiff Flagstad <gautegf@stud.ntnu.no>
2024-08-21 13:48:27 +0200

    fix: formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,20 @@ import Navbar from '@/components/navbar';
 export default function Home() {
   return (
     <main>
-<Grid
-  templateAreas={`"nav main"
-                  "nav footer"`}
-  gridTemplateRows={'100vh 1fr 0px'}
-  gridTemplateColumns={'23vw 1fr'}
-  h='100vh'
-  color='blackAlpha.700'
-  fontWeight='bold'
->
-  <Navbar />
-  <GridItem pl='2' bg='#31363F' area={'main'}>
-    Main
-  </GridItem>
-</Grid>
+      <Grid
+        templateAreas={`"nav main"
+                        "nav footer"`}
+        gridTemplateRows={'100vh 1fr 0px'}
+        gridTemplateColumns={'23vw 1fr'}
+        h='100vh'
+        color='blackAlpha.700'
+        fontWeight='bold'
+      >
+        <Navbar />
+        <GridItem pl='2' bg='#31363F' area={'main'}>
+          Main
+        </GridItem>
+      </Grid>
     </main>
   );
 }
