73687ab5635a2571ee3678fb4a9f34bc0bd599e2
Gaute Greiff Flagstad <gautegf@stud.ntnu.no>
2024-09-13 08:40:16 +0200

    updated layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-"use client";
-import { Grid, GridItem } from "@chakra-ui/react";
-import Navbar from "@/components/navbar";
-export default function Home() {
-  return (
-    <main>
-      <Grid
-        templateAreas={`"nav main"
-                        "nav footer"`}
-        gridTemplateRows={"100vh 1fr 0px"}
-        gridTemplateColumns={"23vw 1fr"}
-        h="100vh"
-        color="blackAlpha.700"
-        fontWeight="bold"
-      >
-        <Navbar />
-        <GridItem pl="2" bg="#31363F" area={"main"}>
-          Main
-        </GridItem>
-      </Grid>
-    </main>
-  );
-}
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const HomePage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/general');
+  }, [router]);
+
+  return null;
+};
+
+export default HomePage;
