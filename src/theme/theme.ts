b099cb78e7d8610f425910e19aed97d0ecbea33a
Gaute Flægstad <gaute.flagstad@gmail.com>
2024-09-16 11:36:58 +0200

    added font in theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+// theme.ts
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Roboto Mono', monospace`,
+    body: `'Roboto Mono', monospace`,
+  },
+});
+
+export default theme;
