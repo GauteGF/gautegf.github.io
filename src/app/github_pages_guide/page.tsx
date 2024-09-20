21d7eee1f24f89c5f6c394fdf467a5f09805cc80
Gaute Flægstad <gaute.flagstad@gmail.com>
2024-09-20 12:51:07 +0200

    ran prettier

--- src/app/github_pages_guide/page.tsx
+++ src/app/github_pages_guide/page.tsx
@@ -51,8 +51,8 @@ const PageDivider = ({ title }: { title: string }) => (
 
 export default function github_pages_guide() {
   return (
-    <main >
-      <Box p={4} bg={"#31363F"} h={"100%"} textColor={"white"} >
+    <main>
+      <Box p={4} bg={"#31363F"} h={"100%"} textColor={"white"}>
         <Text fontSize="3xl" mb={5}>
           Creating a portfolio{" "}
         </Text>
